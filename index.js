acfb7cfc2dc97c8732afc5fef0f50b9d9044f08c
Eder <eder.arodriguez1999@gmail.com>
2025-06-25 21:51:49 -0600

    Configurando Express

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('golaaa como vas hoy');
+import express from 'express';
+import colors from 'colors';
+
+const app = express();
+
+
+
+const port = process.env.PORT || 4000
+
+app.listen(port, () => {
+    console.log( colors.bgBlue.italic(`Servidor backend corriendo en el puerto ${port}`));
+    
+})
