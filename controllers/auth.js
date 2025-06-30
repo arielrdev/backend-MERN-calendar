e4040bccedb273dd9dce9da5b8f574a32f0996df
Eder <eder.arodriguez1999@gmail.com>
2025-06-30 14:24:42 -0600

    [controller/auth.js] - Login de usuario

--- controllers/auth.js
+++ controllers/auth.js
@@ -40,16 +40,45 @@ export const crearUsuario = async ( req, res = response ) => {
     }
 }
 
-export const loginUsuario = ( req, res = response ) => {
+export const loginUsuario = async ( req, res = response ) => {
 
     const { email, password } = req.body;
 
-    res.json({
-        ok: true,
-        msg: 'login',
-        email,
-        password
-    })
+    try {
+        const usuario = await Usuario.findOne({ email });
+        
+        if ( !usuario ) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'El usuario no existe con ese email'
+            })
+        }
+
+        //* Confirmar los passwords
+        const validPassword = bcrypt.compareSync( password, usuario.password );
+        if( !validPassword ) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'Password incorrecto'
+            });
+        }
+
+        //* TODO - Generar JWT
+        
+        res.json({
+            ok: true,
+            uid: usuario.id,
+            name: usuario.name
+        })
+
+    } catch (error) {
+        console.log(error);
+        
+        res.status(500).json({
+            ok: false,
+            msg: 'Por favor hable con el administrador',
+        })
+    }
 }
 
 export const revalidarToken = ( req, res = response ) => {
