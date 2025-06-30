6cf72ad10ae71887ee76e88598b0b3ad51c3f6cb
Eder <eder.arodriguez1999@gmail.com>
2025-06-30 11:34:29 -0600

    [ controllers/auth.js] - Se valida los campos que se recibe del backend con Express Validator

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,21 +1,51 @@
 import { response } from "express"
+import { validationResult } from "express-validator";
 
 
 export const crearUsuario = ( req, res = response ) => {
-    res.json({
+
+    const { name, email, password } = req.body;
+
+    const errors = validationResult( req );
+
+    if ( !errors.isEmpty() ) {
+        return res.status(400).json({
+            ok: false,
+            errors: errors.mapped()
+        });
+    }    
+
+    res.status(201).json({
         ok: true,
-        msg: 'registro'
+        msg: 'registro',
+        name,
+        email,
+        password
     })
 }
 
-export const loginUsuario = (req, res) => {
+export const loginUsuario = ( req, res = response ) => {
+
+    const { email, password } = req.body;
+
+    const errors = validationResult( req );
+
+    if ( !errors.isEmpty() ) {
+        return res.status(400).json({
+            ok: false,
+            errors: errors.mapped()
+        })
+    }
+
     res.json({
         ok: true,
-        msg: 'login'
+        msg: 'login',
+        email,
+        password
     })
 }
 
-export const revalidarToken = (req, res) => {
+export const revalidarToken = ( req, res = response ) => {
     res.json({
         ok: true,
         msg: 'renew'
--- routes/auth.js
+++ routes/auth.js
@@ -4,13 +4,27 @@
  */
 
 import { Router } from "express";
+import { check } from "express-validator";
 import { crearUsuario, loginUsuario, revalidarToken } from '../controllers/auth.js';
 const router = Router();
 
 
-router.post('/new', crearUsuario);
+router.post(
+    '/new', 
+    [// middlewares
+        check('name', 'El nombre es obligatorio').not().isEmpty(),
+        check('email', 'El correo es obligatorio').isEmail(),
+        check('password', 'El password debe ser de 6 caracteres').isLength({ min: 6 }),
+    ], 
+    crearUsuario);
 
-router.post('/', loginUsuario);
+router.post(
+    '/', 
+    [
+        check('email', 'El correo es obligatorio').isEmail(),
+        check('password', 'El password debe ser de 6 caracteres').isLength({ min: 6 }),
+    ], 
+    loginUsuario);
 
 router.get('/renew', revalidarToken);
 
