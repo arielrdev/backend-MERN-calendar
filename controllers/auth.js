04384b0a5a8eab4132ba504a0e66500a7b3b8bde
Eder <eder.arodriguez1999@gmail.com>
2025-06-30 11:52:16 -0600

    created: [middlewares/validar-campos.js] - Custom Middlewares

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,20 +1,10 @@
 import { response } from "express"
-import { validationResult } from "express-validator";
 
 
 export const crearUsuario = ( req, res = response ) => {
 
     const { name, email, password } = req.body;
-
-    const errors = validationResult( req );
-
-    if ( !errors.isEmpty() ) {
-        return res.status(400).json({
-            ok: false,
-            errors: errors.mapped()
-        });
-    }    
-
+    
     res.status(201).json({
         ok: true,
         msg: 'registro',
@@ -27,15 +17,6 @@ export const crearUsuario = ( req, res = response ) => {
 export const loginUsuario = ( req, res = response ) => {
 
     const { email, password } = req.body;
-
-    const errors = validationResult( req );
-
-    if ( !errors.isEmpty() ) {
-        return res.status(400).json({
-            ok: false,
-            errors: errors.mapped()
-        })
-    }
 
     res.json({
         ok: true,
--- routes/auth.js
+++ routes/auth.js
@@ -6,6 +6,7 @@
 import { Router } from "express";
 import { check } from "express-validator";
 import { crearUsuario, loginUsuario, revalidarToken } from '../controllers/auth.js';
+import { validarCampos } from "../middlewares/validar-campos.js";
 const router = Router();
 
 
@@ -15,6 +16,7 @@ router.post(
         check('name', 'El nombre es obligatorio').not().isEmpty(),
         check('email', 'El correo es obligatorio').isEmail(),
         check('password', 'El password debe ser de 6 caracteres').isLength({ min: 6 }),
+        validarCampos
     ], 
     crearUsuario);
 
@@ -23,6 +25,7 @@ router.post(
     [
         check('email', 'El correo es obligatorio').isEmail(),
         check('password', 'El password debe ser de 6 caracteres').isLength({ min: 6 }),
+        validarCampos
     ], 
     loginUsuario);
 
