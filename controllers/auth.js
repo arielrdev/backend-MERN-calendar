2b2906d62e2e0e597ca19fd4a2119bad6106a33b
Eder <eder.arodriguez1999@gmail.com>
2025-06-30 13:52:36 -0600

    Validacion y creacion de usuarios en la BD

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,17 +1,38 @@
 import { response } from "express"
+import Usuario from "../models/Usuario.js"
 
 
-export const crearUsuario = ( req, res = response ) => {
+export const crearUsuario = async ( req, res = response ) => {
 
-    const { name, email, password } = req.body;
-    
-    res.status(201).json({
-        ok: true,
-        msg: 'registro',
-        name,
-        email,
-        password
-    })
+    const { email, password } = req.body;
+
+    try {
+        let usuario = await Usuario.findOne({ email });
+        
+        if ( usuario ) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'Correo ya registrado'
+            })
+        }
+
+        usuario = new Usuario( req.body );   
+        await usuario.save();
+
+        res.status(201).json({
+            ok: true,
+            uid: usuario.id,
+            name: usuario.name
+        })
+
+    } catch (error) {
+        console.log(error);
+        
+        res.status(500).json({
+            ok: false,
+            msg: 'Por favor hable con el administrador',
+        })
+    }
 }
 
 export const loginUsuario = ( req, res = response ) => {
--- models/Usuario.js
+++ models/Usuario.js
@@ -1,6 +1,21 @@
-import { Schema } from "mongoose";
+import mongoose, { Schema } from "mongoose";
 
 
 const UsuarioSchema = Schema({
-    
-})
+    name: {
+        type: String,
+        require: true
+    },
+    email: {
+        type: String,
+        require: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        require: true
+    }
+});
+
+const Usuario = mongoose.model('Usuario', UsuarioSchema);
+export default Usuario;
